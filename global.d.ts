ac579129229de245a10ec23fc8aeffccfbc8ed6e
garadieu <garadieu@gmail.com>
2026-06-08 20:08:09 +0700

    chore: configure build dependencies, tsconfig, typings, and gitignore patterns

---
+++ global.d.ts
@@ -0,0 +1,8 @@
+declare module 'viem';
+declare module 'viem/chains';
+declare module 'viem/accounts';
+declare module 'viem/account-abstraction';
+declare module '@circle-fin/bridge-kit';
+declare module '@circle-fin/adapter-viem-v2';
+declare module '@circle-fin/modular-wallets-core';
+declare module 'd3-sankey';
